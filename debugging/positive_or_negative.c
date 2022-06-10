6f75ac4a65590f45f9ca60df1070da2e63bf761d
crasride <jose.dfernandez@yahoo.fr>
2022-06-10 08:15:35 +0000

    Multiple mains

--- debugging/positive_or_negative.c
+++ debugging/positive_or_negative.c
@@ -2,17 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - program entry
-* printf - print the message
+* positive_or_negative - gives the correct output
+* @n: oprenad
 * Return: 0
 */
-int main(void)
+int positive_or_negative(int n)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 		printf("%d is positive\n", n);
 
