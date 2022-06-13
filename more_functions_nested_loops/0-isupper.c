d5a51749d42605f740ca766d797a2a9e4b2a82b3
crasride <jose.dfernandez@yahoo.fr>
2022-06-13 09:42:51 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,21 +2,16 @@
 /*
 * _isupper - function
 * @c: operand
-* Description: that checks for uppercase character.
 * Return: 1 if uppercase, 0 otherwise
 */
 int _isupper(int c)
 {
 	while (c == 'A')
 	{
-	_putchar(':');
-	_putchar(' ');
 	return (1);
 	}
 	while (c == 'a')
 	{
-	_putchar(':');
-	_putchar(' ');
 	return (0);
 	}
 	return (c);
