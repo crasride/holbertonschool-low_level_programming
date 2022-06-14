0804c71a42ccff7570b5181fff21b37baa8d6d02
crasride <jose.dfernandez@yahoo.fr>
2022-06-14 12:01:09 +0000

    98 Battery st

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - function
+* @n: pointer
+*
+* Return: Always 0
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;  /*pointer to an int as parameter and updates the value*/
+}
+
