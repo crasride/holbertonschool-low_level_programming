9cc11b84e82137a805879f4957b1e8f994d3a74b
crasride <jose.dfernandez@yahoo.fr>
2022-06-23 08:18:53 +0000

    that adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
 * main - that adds positive numbers
 * @argc: number counts arguments
@@ -8,17 +9,23 @@
 */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int r = 0, i, s;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i])) /* non num */
+		for (s = 0; argv[i][s]; s++)
 		{
-			printf("Error\n");
-			return (1);
+			if (isdigit(argv[i][s]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	for (i = 1; i < argc; i++)
+	{
+		r += atoi(argv[i]);
+	}
+	printf("%d\n", r);
 	return (0);
 }
