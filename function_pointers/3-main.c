da8b6295602ef399211af25af025f2798b9d084d
crasride <jose.dfernandez@yahoo.fr>
2022-06-30 12:08:55 +0000

    contain your main function only

---
+++ function_pointers/3-main.c
@@ -0,0 +1,21 @@
+#include "3-calc.h"
+
+/**
+* main - performs simple operations
+* @argc: number of arguments passed
+* @argv: array of pointers to arguments
+*
+* Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+
+}
