f05dc03560ec6c36a38482d68c759c687a70e1de
crasride <jose.dfernandez@yahoo.fr>
2022-07-04 08:40:17 +0000

    Protoype function

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+* sum_them_all - function
+* @n: number  arguments
+* @...: arguments
+* Description: that returns the sum of all its parameters
+* Return: sum / n == 0, return 0
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, sum = 0;
+
+	va_start(args, n);
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, const unsigned int);
+	}
+	va_end(args);
+	return (sum);
+}
