60676071df7dadc61d7d938c51ed5b90a2abee94
crasride <jose.dfernandez@yahoo.fr>
2022-07-20 20:42:33 +0200

    Create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+* _strlen - finds the length of a string
+* @str: pointer to the string
+*
+* Return: length of the string
+*/
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
 * create_file -  function
 * @filename: filename to create
 * @text_content: text copy into new file
@@ -10,18 +25,21 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, failure, len;
+	int fd;
+	ssize_t len = 0;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC,  S_IRUSR | S_IWUSR);
-	if (fd < 0)
-		return (-1);
-	while (text_content && *(text_content + len))
-		len++;
-	failure = write(fd, text_content, len);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd == -1)
+			return (-1);
+
+		if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
 		close(fd);
-	if (failure < 0)
+
+		if (len == -1)
 		return (-1);
 	return (1);
 }
