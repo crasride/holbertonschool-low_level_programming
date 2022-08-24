2555d4b77874a5e85aff05495764ac447dba7bb8
crasride <jose.dfernandez@yahoo.fr>
2022-08-24 08:21:53 +0200

    Prototype Functions

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALG
+#define SEARCH_ALG
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Prototype Functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /* SEARCH_ALG */
